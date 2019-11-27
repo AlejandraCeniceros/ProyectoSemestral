c89eae138eaaffc7ecb2388b04fad557f209c6c0
Daniela Alejandra Ceniceros G <dcac@outlook.es>
2019-11-26 18:09:57 -0700

    va

---
+++ ProyectoSemestral/Info.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemestral
+{
+    class Info
+    {
+        public string Titulo { get; set; }
+        public int Año { get; set; }
+        public string Director { get; set; }
+        public string Genero { get; set; }
+        public int Temporadas { get; set; }
+        public string Sinopsis { get; set; }
+        public int Rating { get; set; }
+    }
+}
--- ProyectoSemestral/MainWindow.xaml.cs
+++ ProyectoSemestral/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace ProyectoSemestral
 {
@@ -20,9 +21,12 @@ namespace ProyectoSemestral
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Info> info = new ObservableCollection<Info>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+       
     }
 }
