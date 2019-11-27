0606da4dec7b313833d7d0377ff8ddc8ac7154f5
AlejandraCeniceros <dcac@outlook.es>
2019-11-27 00:03:31 -0800

    estoy teniendo una crisis existencial

--- ProyectoSemestral/Agregar.xaml.cs
+++ ProyectoSemestral/Agregar.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace ProyectoSemestral
 {
     /// <summary>
@@ -22,18 +23,27 @@ namespace ProyectoSemestral
         public Agregar()
         {
             InitializeComponent();
+            Btn_Pelicula.IsChecked = true;
         }
 
         private void Btn_Pelicula_Checked(object sender, RoutedEventArgs e)
         {
-            if (Btn_Pelicula.IsChecked == true){
+            if (Btn_Pelicula.IsChecked == true)
+            {
+               
+                lbl_Temporadas.Visibility = Visibility.Hidden;
+                box_Temporadas.Visibility = Visibility.Hidden;
+            }
 
 
         }
 
         private void Btn_Serie_Checked(object sender, RoutedEventArgs e)
         {
-
-        }
+            if(Btn_Serie.IsChecked == true){
+                
+                lbl_Temporadas.Visibility = Visibility.Visible;
+                box_Temporadas.Visibility = Visibility.Visible;
+;        }
     }
 }
